3c4226aa9da1bde6fb97658602797574e909064d
Paco Martín Fernández <erespia2@gmail.com>
2013-07-01 10:54:39 +0100

    Ejemplo de utilizacin Google Places

---
+++ src/gdgtenerife/tlp/apis/mapas/IStandardTaskListener.java
@@ -0,0 +1,11 @@
+package gdgtenerife.tlp.apis.mapas;
+
+/**
+ * Adaptación de erespia2@gmail.com de una interfaz creada por:
+ * @author Dinesh Harjani (goldrunner192287@gmail.com)
+ * GeoBloc, ULL junto a jelcaf@gmail.com
+ *
+ */
+public interface IStandardTaskListener {
+    void taskComplete(Object result);
+}
--- src/gdgtenerife/tlp/apis/mapas/MainActivity.java
+++ src/gdgtenerife/tlp/apis/mapas/MainActivity.java
@@ -4,112 +4,118 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends FragmentActivity { // TODO: Explicar este cambio
+public class MainActivity extends FragmentActivity {
 
-	// TODO: Explicar esto
 	private GoogleMap mapa = null;
-	private int vista = 0;
+	private AutoCompleteTextView lugar = null;
+	private DetailsPlaceOne geoLugar;
+	private FillPlace buscarLugar;
 	private Marker marcador=null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mapa = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 		
 		// TODO: Explicar Esto
-		mapa = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {	
-		switch(item.getItemId())
-		{
-			case R.id.menu_vista:
-				alternarVista();
-				break;
-			case R.id.menu_mover:
-				//Centramos el mapa en la TLP
-				CameraUpdate camUpd1 = CameraUpdateFactory.newLatLng(new LatLng(28.454532710377187, -16.257910238360633));
-				mapa.moveCamera(camUpd1);
-				break;
-			case R.id.menu_animar:
-				//Centramos el mapa en la TLP y con nivel de zoom 5
-				CameraUpdate camUpd2 = CameraUpdateFactory.newLatLngZoom(new LatLng(28.454532710377187, -16.257910238360633), 5F);
-				mapa.animateCamera(camUpd2);
-				break;
-			case R.id.menu_3d:
-				LatLng recintoFerial = new LatLng(28.454532710377187, -16.257910238360633);
-				CameraPosition camPos = new CameraPosition.Builder()
-					    .target(recintoFerial)   //Centramos el mapa en la TLP
-					    .zoom(17)         //Establecemos el zoom en 19
-					    .bearing(45)      //Establecemos la orientación con el noreste arriba
-					    .tilt(70)         //Bajamos el punto de vista de la cámara 70 grados
-					    .build();
-
-				CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
-
-				mapa.animateCamera(camUpd3);
-				break;
-			case R.id.menu_posicion:
-				CameraPosition camPos2 = mapa.getCameraPosition();
-				LatLng pos = camPos2.target;
-				Toast.makeText(MainActivity.this, 
-						"Lat: " + pos.latitude + " - Lng: " + pos.longitude, 
-						Toast.LENGTH_LONG).show();
-				break;
-			case R.id.menu_marcador:
-				if (marcador==null)
-					marcador = mapa.addMarker(new MarkerOptions()
-		        		.position(new LatLng(28.454532710377187, -16.257910238360633))
-		        		.title("Taller GDG Tenerife en la TLP"));
-				else{
-					marcador.remove();
-					marcador=null;
+		lugar = (AutoCompleteTextView) findViewById(R.id.editTextPlace);
+		
+		// Preparamos el adaptador usado para mostrar las propuestas de autocompletado, en este caso una lista
+		final ArrayAdapter<String> adapterFrom = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+		adapterFrom.setNotifyOnChange(true);
+		lugar.setAdapter(adapterFrom);
+		// Monitorizamos el evento de cambio en el campo a autocompletar para buscar las propuestas de autocompletado
+		lugar.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// Calculamos el autocompletado
+				if (count == 0){
+					if (marcador != null)
+						marcador.remove();
+				} else if (count%3 == 1){
+					adapterFrom.clear();
+					// Ejecutamos en segundo plano la busqueda de propuestas de autocompletado
+					buscarLugar = new FillPlace(adapterFrom, lugar, getBaseContext());
+					buscarLugar.execute(lugar.getText().toString());
 				}
-				
-				break;
-		}
+			}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			public void afterTextChanged(Editable s) {}
+		});
+		
+		lugar.setOnItemClickListener(new OnItemClickListener() {
 
-		return super.onOptionsItemSelected(item);
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				geoLugar = new DetailsPlaceOne();
+		        // TODO: ¡CUIDADO!
+		        while(buscarLugar.referencesPlace.isEmpty()){}
+		        geoLugar.setListener(new PlaceToPointMap_TaskListener(lugar.getText().toString()));
+				geoLugar.execute(buscarLugar.referencesPlace.get(lugar.getText().toString()));
+			}
+
+        });
+
 	}
 	
-	private void alternarVista() {
-		vista = (vista + 1) % 4;
+	private class PlaceToPointMap_TaskListener implements IStandardTaskListener {
 
-		switch(vista)
-		{
-			case 0:
-				mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-				break;
-			case 1:
-				mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-				break;
-			case 2:
-				mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-				break;
-			case 3:
-				mapa.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-				break;
-		}
+        private String markerStr;
+        
+        public PlaceToPointMap_TaskListener(String markerStr) {
+        	this.markerStr =  markerStr;
+        }
+        
+        @Override
+        public void taskComplete(Object result) {
+        	if ((Boolean)result){
+        		// Recogemos la posicion que hemos pedido al Web Service de Google Place
+        		Double lat = geoLugar.coordinates.get("lat");
+        		Double lng = geoLugar.coordinates.get("lng");
+        		LatLng go = new LatLng(lat, lng);
+        		
+        		// Creamos la animación de movimiento hacia el lugar que hemos introducido
+        		CameraPosition camPos = new CameraPosition.Builder()
+	                .target(go)
+	                .zoom(17)         //Establecemos el zoom en 17
+	                .tilt(45)         //Bajamos el punto de vista de la cámara 70 grados
+	                .build();
+         
+        		// Lanzamos el movimiento
+        		CameraUpdate camUpd = CameraUpdateFactory.newCameraPosition(camPos);
+        		mapa.animateCamera(camUpd);
+        		
+        		// Insertamos el Marcador
+        		mapa.addMarker(new MarkerOptions()
+    	        	.position(new LatLng(lat, lng))
+    	        	.title(markerStr)
+    	        	.draggable(true)
+    	        	.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        		
+        		// Esconder teclado para que se vea la animación del mapa
+        		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
+        	}
+        }
 	}
 
 }
